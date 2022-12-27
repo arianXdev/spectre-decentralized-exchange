b258a15f0f584f7f165c3fb2019be27bfb5e34f5
Arian Hosseini <arianhosseini@outlook.com>
2022-12-27 22:40:17 +0330

    import dotenv library in the Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
 };
