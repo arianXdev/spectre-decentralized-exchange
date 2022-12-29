993239c5c9f4e253fd7cd6336a6874a05f0ed422
Arian Hosseini <arianhosseini@outlook.com>
2022-12-29 17:52:01 +0330

    created the Smart Contract of Spectre Token

---
+++ contracts/SpectreToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+/**
+ * @title Spectre Token
+ * @author Arian Hosseini
+ * @notice A Simple ERC-20 Token
+ */
+contract SpectreToken {
+    /// @notice EIP-20 token name for this token
+    string public constant name = "Spectre";
+
+    /// @notice EIP-20 token symbol for this token
+    string public constant symbol = "SPEC";
+}
