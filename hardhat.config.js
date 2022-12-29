681bf08c6b164a885d1aa28e8961a58c12f27c90
Arian Hosseini <arianhosseini@outlook.com>
2022-12-29 18:13:29 +0330

    set up default Hardhat network connection

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: "0.8.17",
+	networks: {
+		localhost: {},
+	},
 };
