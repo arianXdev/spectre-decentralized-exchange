6403a58f90e28012cd1c0c1252bd04105a87602e
Arian Hosseini <arianhosseini@outlook.com>
2023-01-02 21:45:28 +0330

    feat: added totalSupply and decimals unit tests

--- test/SpectreToken.test.js
+++ test/SpectreToken.test.js
@@ -11,12 +11,19 @@ beforeEach(async () => {
 
 test("Token Name should be Spectre", async () => {
 	const name = await spectreToken.name();
-
 	expect(name).toBe("Spectre");
 });
 
 test("Token Symbol should be SPEC", async () => {
 	const symbol = await spectreToken.symbol();
-
 	expect(symbol).toBe("SPEC");
+});
+
+test("Token decimals should be 18", async () => {
+	expect(await spectreToken.decimals()).toStrictEqual(18);
+});
+
+test("Total number of tokens in circulation should be 1 million SPEC", async () => {
+	const value = Number(ethers.utils.parseUnits("1000000", "ether")); // convert 1 million ether to wei
+	expect(Number(await spectreToken.totalSupply())).toStrictEqual(value);
 });
