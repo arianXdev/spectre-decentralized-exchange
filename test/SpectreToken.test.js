9a3272932ca09bf09c91eac8315534f9baab3cf5
Arian Hosseini <arianhosseini@outlook.com>
2023-01-02 19:53:36 +0330

    wrote the first test to check the Token name

--- test/SpectreToken.test.js
+++ test/SpectreToken.test.js
@@ -1,1 +1,11 @@
 const { ethers } = require("hardhat");
+
+test("Token Name should be Spectre", async () => {
+	// Fetch the contract using ethers.js
+	const SpectreToken = await ethers.getContractFactory("SpectreToken");
+	const spectreToken = await SpectreToken.deploy();
+
+	const name = await spectreToken.name();
+
+	expect(name).toBe("Spectre");
+});
