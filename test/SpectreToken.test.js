ea95a8d2edda4b543c66ca4b763d68504e69dcc7
Arian Hosseini <arianhosseini@outlook.com>
2023-01-02 20:26:44 +0330

    refactor: refactor unit tests

--- test/SpectreToken.test.js
+++ test/SpectreToken.test.js
@@ -1,19 +1,21 @@
 const { ethers } = require("hardhat");
 
-test("Token Name should be Spectre", async () => {
+let spectreToken;
+
+// beforeEach method will run before tests
+beforeEach(async () => {
 	// Fetch the contract using ethers.js
 	const SpectreToken = await ethers.getContractFactory("SpectreToken");
-	const spectreToken = await SpectreToken.deploy();
+	spectreToken = await SpectreToken.deploy();
+});
 
+test("Token Name should be Spectre", async () => {
 	const name = await spectreToken.name();
 
 	expect(name).toBe("Spectre");
 });
 
 test("Token Symbol should be SPEC", async () => {
-	const SpectreToken = await ethers.getContractFactory("SpectreToken");
-	const spectreToken = await SpectreToken.deploy();
-
 	const symbol = await spectreToken.symbol();
 
 	expect(symbol).toBe("SPEC");
