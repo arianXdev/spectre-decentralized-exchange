37619214ba3578070d6fa419d24cbf9cb06d2962
Arian Hosseini <arianhosseini@outlook.com>
2023-01-02 22:36:50 +0330

    feat: created a configurable ERC-20 Token

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+/**
+ * @title Configurable Token
+ * @author Arian Hosseini
+ * @notice A Simple ERC-20 Token
+ */
+contract SpectreToken {
+    /// @notice EIP-20 token name for this token
+    string public name;
+
+    /// @notice EIP-20 token symbol for this token
+    string public symbol;
+
+    /// @notice EIP-20 token decimals for this token
+    uint8 public decimals;
+
+    /// @notice Total number of tokens in circulation
+    uint256 public totalSupply;
+
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+    }
+}
