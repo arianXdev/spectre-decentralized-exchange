c7d34de33f6e3f34b9a3a91c540eb244fc15dfd1
Arian Hosseini <arianhosseini@outlook.com>
2023-01-02 22:56:13 +0330

    refactor: refactor SpectreToken tests

--- test/SpectreToken.test.js
+++ test/SpectreToken.test.js
@@ -4,28 +4,31 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-let spectreToken;
-// beforeEach method will run before tests
-beforeEach(async () => {
-	// Fetch the contract using ethers.js
-	const SpectreToken = await ethers.getContractFactory("SpectreToken");
-	spectreToken = await SpectreToken.deploy();
-});
+describe("Spectre Token Deployment", () => {
+	let spectreToken;
 
-test("Token Name should be Spectre", async () => {
-	const name = await spectreToken.name();
-	expect(name).toBe("Spectre");
-});
+	// beforeEach method will run before tests
+	beforeEach(async () => {
+		// Fetch the contract using ethers.js
+		const SpectreToken = await ethers.getContractFactory("SpectreToken");
+		spectreToken = await SpectreToken.deploy();
+	});
 
-test("Token Symbol should be SPEC", async () => {
-	const symbol = await spectreToken.symbol();
-	expect(symbol).toBe("SPEC");
-});
+	test("Token Name should be Spectre", async () => {
+		const name = await spectreToken.name();
+		expect(name).toBe("Spectre");
+	});
 
-test("Token decimals should be 18", async () => {
-	expect(await spectreToken.decimals()).toStrictEqual(18);
-});
+	test("Token Symbol should be SPEC", async () => {
+		const symbol = await spectreToken.symbol();
+		expect(symbol).toBe("SPEC");
+	});
 
-test("Total number of tokens in circulation should be 1 million SPEC", async () => {
-	expect(Number(await spectreToken.totalSupply())).toStrictEqual(Number(tokens(1000000)));
+	test("Token decimals should be 18", async () => {
+		expect(await spectreToken.decimals()).toStrictEqual(18);
+	});
+
+	test("Total number of tokens in circulation should be 1 million SPEC", async () => {
+		expect(Number(await spectreToken.totalSupply())).toStrictEqual(Number(tokens(1000000)));
+	});
 });
