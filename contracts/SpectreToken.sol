71483e884d855c21823745ad61ea4a1f9710de8b
Arian Hosseini <arianhosseini@outlook.com>
2023-01-03 20:06:09 +0330

    feat: added balances and balanceOf function of SpectreToken

--- contracts/SpectreToken.sol
+++ contracts/SpectreToken.sol
@@ -20,4 +20,20 @@ contract SpectreToken {
 
     /// @notice Total number of tokens in circulation
     uint256 public totalSupply = 1000000e18; // 1 million SPEC
+
+	/// @notice Official record of token balances for each account
+    mapping (address => uint256) internal balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+	/**
+		* @notice Get the number of tokens held by the `account`
+		* @param account The address of the account to get the balance of
+		* @return The number of tokens held
+	*/
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
 }
