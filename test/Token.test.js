7d99849b20b5141088446605508176c21673c844
Arian Hosseini <arianhosseini@outlook.com>
2023-01-04 20:53:54 +0330

    test: added transfer function tests

--- test/Token.test.js
+++ test/Token.test.js
@@ -4,8 +4,8 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("Token Contract Deployment", () => {
-	let token, accounts, deployer;
+describe("Token", () => {
+	let token, accounts, deployer, receiver;
 
 	const name = "Arian";
 	const symbol = "ARN";
@@ -21,26 +21,51 @@ describe("Token Contract Deployment", () => {
 		// Get all the ethereum accounts
 		accounts = await ethers.getSigners();
 		// Get the deployer / owner account
-		deployer = accounts[0].address;
+		deployer = accounts[0];
+		// Get the receiver / customer account
+		receiver = accounts[1];
 	});
 
-	test("Token Name should be correct", async () => {
-		expect(await token.name()).toBe(name);
+	describe("Contract Deployment", () => {
+		test("Token Name should be correct", async () => {
+			expect(await token.name()).toBe(name);
+		});
+
+		test("Token Symbol should be correct", async () => {
+			expect(await token.symbol()).toBe(symbol);
+		});
+
+		test("Token decimals should be 18", async () => {
+			expect(await token.decimals()).toStrictEqual(decimals);
+		});
+
+		test("Total number of tokens in circulation should be correct", async () => {
+			expect(Number(await token.totalSupply())).toStrictEqual(Number(tokens(totalSupply)));
+		});
+
+		test("Assign totalSupply to deployer (owner)", async () => {
+			expect(await token.balanceOf(deployer.address)).toStrictEqual(tokens(totalSupply));
+		});
 	});
 
-	test("Token Symbol should be correct", async () => {
-		expect(await token.symbol()).toBe(symbol);
-	});
+	describe("Transfering Tokens", () => {
+		let amount, transaction, result;
 
-	test("Token decimals should be 18", async () => {
-		expect(await token.decimals()).toStrictEqual(decimals);
-	});
+		beforeEach(async () => {
+			amount = tokens(100); // 100 ARN
 
-	test("Total number of tokens in circulation should be correct", async () => {
-		expect(Number(await token.totalSupply())).toStrictEqual(Number(tokens(totalSupply)));
-	});
+			// Transfer tokens
+			transaction = await token.connect(deployer).transfer(receiver.address, amount); // connects deployer to the Token smart contract, so it can sign the trxs
+			result = await transaction.wait(); // wait for trx to get included and add into the Blockchain
+		});
 
-	test("Assign totalSupply to deployer (owner)", async () => {
-		expect(await token.balanceOf(deployer)).toStrictEqual(tokens(totalSupply));
+		test("Transfer tokens should be done", async () => {
+			// Log the balance before transfer
+			// console.log("Deployer balance before transfer ", ethers.utils.formatEther(await token.balanceOf(deployer.address)), " ARN");
+			// console.log("Receiver balance before transfer ", ethers.utils.formatEther(await token.balanceOf(receiver.address)), " ARN");
+
+			expect(await token.balanceOf(deployer.address)).toEqual(tokens(999900));
+			expect(await token.balanceOf(receiver.address)).toEqual(amount);
+		});
 	});
 });
