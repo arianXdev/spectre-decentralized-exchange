a408c3dd2faeabe7fe689b67be2a1cd461fe3ad1
Arian Hosseini <arianhosseini@outlook.com>
2023-01-08 20:42:20 +0330

    refactor: added require 0 addresses

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -55,6 +55,7 @@ contract Token {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         // Require that sender has enough tokens to spend
         require(balanceOf(msg.sender) >= _value, "Not enough tokens!");
+        require(_to != address(0), "invalid address!");
 
         // Deduct tokens from the spender
         balances[msg.sender] -= _value;
@@ -68,6 +69,8 @@ contract Token {
     /// @notice approve function allows _spender (third-party) to withdraw from your account multiple times, up to the _value (using transferFrom)
     // the preson who's approving and calling this function is always msg.sender
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "invalid address!");
+
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
