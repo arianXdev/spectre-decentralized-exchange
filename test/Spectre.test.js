8f2fd8c187beafc85b2defd791efaa049423b662
Arian Hosseini <arianhosseini@outlook.com>
2023-01-11 20:53:57 +0330

    test: added tracking feet account unit test

--- test/Spectre.test.js
+++ test/Spectre.test.js
@@ -1,14 +1,22 @@
 const { ethers } = require("hardhat");
 
 describe("Spectre Exchange", () => {
-	let spectre, accounts;
+	let spectre, accounts, deployer, feeAccount;
 
 	beforeEach(async () => {
-		// Deploy the exchange contract
-		const SpectreExchange = await ethers.getContractFactory("Spectre");
-		spectre = await SpectreExchange.deploy();
-
 		// Get all the accounts
 		accounts = await ethers.getSigners();
+		deployer = await accounts[0];
+		feeAccount = await accounts[1];
+
+		// Deploy the exchange contract
+		const SpectreExchange = await ethers.getContractFactory("Spectre");
+		spectre = await SpectreExchange.deploy(feeAccount.address);
+	});
+
+	describe("Deployment", () => {
+		test("tracks the fee account", async () => {
+			expect(await spectre.feeAccount()).toBe(feeAccount.address);
+		});
 	});
 });
