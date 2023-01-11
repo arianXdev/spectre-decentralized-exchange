0045168402e13424f139f0678bf5ffce854d3e9d
Arian Hosseini <arianhosseini@outlook.com>
2023-01-11 20:35:28 +0330

    feat: created Spectre Exchange Smart Contract

---
+++ contracts/Spectre.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Spectre Decentralized Exchange
+ * @author Arian Hosseini
+ * @notice the smart contract that handles all the functionalities of Spectre DEX
+ */
+contract Spectre { 
+
+}
