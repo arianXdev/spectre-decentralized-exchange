df3e03187380f9413d94a39079d7d45441f1db14
Arian Hosseini <arianhosseini@outlook.com>
2023-01-12 21:06:18 +0330

    feat: Implement DEPOSIT feature of the Spectre DEX

--- contracts/Spectre.sol
+++ contracts/Spectre.sol
@@ -23,4 +23,24 @@ contract Spectre {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+
+    /// @notice deposit lets users deposit their token into the exchange
+    function deposit(address _tokenAddress, uint256 _amount) public {
+        if (_tokenAddress == address(Token(_tokenAddress))) {
+            require(Token(_tokenAddress).transferFrom(msg.sender, address(this), _amount));
+            depositedTokens[_tokenAddress][msg.sender] += _amount;
+            
+        } else if (_tokenAddress == address(SpectreToken(_tokenAddress))) {
+            
+            require(SpectreToken(_tokenAddress).transferFrom(msg.sender, address(this), _amount));
+            depositedTokens[_tokenAddress][msg.sender] += _amount;
+        }
+
+        emit Deposit(_tokenAddress, msg.sender, _amount, depositedTokens[_tokenAddress][msg.sender]);
+    } 
+
+    /// @notice get the deposited amount of each user for all tokens
+     function balanceOf(address _token, address _user) public view returns (uint256 depositedBalance) {
+        return depositedTokens[_token][_user];
+    }
 }
