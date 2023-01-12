1fddeba9566528188acd6f19888cef84b32a5bd9
Arian Hosseini <arianhosseini@outlook.com>
2023-01-12 21:06:55 +0330

    test: added all the tests related to Deposit feature of the exchange

--- test/Spectre.test.js
+++ test/Spectre.test.js
@@ -1,19 +1,40 @@
 const { ethers } = require("hardhat");
 
+const convertTokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
 describe("Spectre Exchange", () => {
-	let spectre, accounts, deployer, feeAccount;
+	let spectre, tether, spectreToken, accounts, deployer, feeAccount, user1, user2;
 
 	const feePercent = 10; // %10
 
 	beforeEach(async () => {
+		const SpectreExchange = await ethers.getContractFactory("Spectre");
+		const SpectreToken = await ethers.getContractFactory("SpectreToken");
+		const Token = await ethers.getContractFactory("Token");
+
 		// Get all the accounts
 		accounts = await ethers.getSigners();
 		deployer = await accounts[0];
 		feeAccount = await accounts[1];
+		user1 = accounts[2];
+		user2 = accounts[3];
 
+		// Deploy mock Tether token
+		tether = await Token.deploy("Tether", "USDT", 1000000);
+		// Deploy Spectre Token
+		spectreToken = await SpectreToken.deploy();
 		// Deploy the exchange contract
-		const SpectreExchange = await ethers.getContractFactory("Spectre");
 		spectre = await SpectreExchange.deploy(feeAccount.address, feePercent);
+
+		// Charge / Give user1 and user2, 100 USDT in order to work with the exchange
+		await tether.connect(deployer).transfer(user1.address, convertTokens(100));
+		await tether.connect(deployer).transfer(user2.address, convertTokens(100));
+
+		// Charge / Give user1 and user2, 100 SPEC in order to interact with the exchange
+		await spectreToken.connect(deployer).transfer(user1.address, convertTokens(100));
+		await spectreToken.connect(deployer).transfer(user2.address, convertTokens(100));
 	});
 
 	describe("Deployment", () => {
@@ -23,6 +44,33 @@ describe("Spectre Exchange", () => {
 
 		test("tracks the fee percent", async () => {
 			expect(Number(await spectre.feePercent())).toEqual(feePercent);
+		});
+	});
+
+	describe("Depositing Tokens", () => {
+		let transaction, result;
+		let amount = convertTokens(10);
+
+		beforeEach(async () => {
+			// Approve transfering tokens (First step)
+			await spectreToken.connect(user1).approve(spectre.address, amount);
+			await tether.connect(user1).approve(spectre.address, amount);
+
+			// Deposit tokens (Second step)
+			transaction = await spectre.connect(user1).deposit(spectreToken.address, amount);
+			transaction = await spectre.connect(user1).deposit(tether.address, amount);
+
+			result = await transaction.wait();
+		});
+
+		test("tracks the SpectreToken (SPEC) deposit", async () => {
+			expect(await spectreToken.balanceOf(spectre.address)).toEqual(amount);
+			expect(await spectre.balanceOf(spectreToken.address, user1.address)).toEqual(amount);
+		});
+
+		test("tracks the Tether (USDT) deposit", async () => {
+			expect(await tether.balanceOf(spectre.address)).toEqual(amount);
+			expect(await spectre.balanceOf(tether.address, user1.address)).toEqual(amount);
 		});
 	});
 });
