275aafb01886754f3d645afe35a6b4c96380d797
Arian Hosseini <arianhosseini@outlook.com>
2023-01-12 21:05:50 +0330

    feat: created depositedTokens and Deposit event

--- contracts/Spectre.sol
+++ contracts/Spectre.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./SpectreToken.sol";
 import "./Token.sol";
+
 /**
  * @title Spectre Decentralized Exchange
  * @author Arian Hosseini
@@ -11,6 +12,12 @@ import "./Token.sol";
 contract Spectre { 
     address public feeAccount;
     uint24 public feePercent;
+
+
+    /// @notice a record of all deposited amounts of each user for all tokens
+    mapping (address => mapping (address => uint256)) internal depositedTokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint24 _feePercent) {
         feeAccount = _feeAccount;
