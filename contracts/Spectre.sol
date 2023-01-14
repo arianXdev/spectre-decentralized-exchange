b8577c116d26b001f77cd37e9e785d08bb86a676
Arian Hosseini <arianhosseini@outlook.com>
2023-01-14 20:26:38 +0330

    feat: implemented Withdraw feature for the exchange

--- contracts/Spectre.sol
+++ contracts/Spectre.sol
@@ -19,6 +19,8 @@ contract Spectre {
     mapping (address => mapping (address => uint256)) internal depositedTokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+
 
     constructor(address _feeAccount, uint24 _feePercent) {
         feeAccount = _feeAccount;
@@ -26,19 +28,34 @@ contract Spectre {
     }
 
     /// @notice deposit lets users deposit their token into the exchange
-    function deposit(address _tokenAddress, uint256 _amount) public {
-        if (_tokenAddress == address(Token(_tokenAddress))) {
-            require(Token(_tokenAddress).transferFrom(msg.sender, address(this), _amount));
-            depositedTokens[_tokenAddress][msg.sender] += _amount;
+    function deposit(address _tokenAddr, uint256 _amount) public {
+        if (_tokenAddr == address(Token(_tokenAddr))) {
+            require(Token(_tokenAddr).transferFrom(msg.sender, address(this), _amount));
+            depositedTokens[_tokenAddr][msg.sender] += _amount;
             
-        } else if (_tokenAddress == address(SpectreToken(_tokenAddress))) {
+        } else if (_tokenAddr == address(SpectreToken(_tokenAddr))) {
             
-            require(SpectreToken(_tokenAddress).transferFrom(msg.sender, address(this), _amount));
-            depositedTokens[_tokenAddress][msg.sender] += _amount;
+            require(SpectreToken(_tokenAddr).transferFrom(msg.sender, address(this), _amount));
+            depositedTokens[_tokenAddr][msg.sender] += _amount;
         }
 
-        emit Deposit(_tokenAddress, msg.sender, _amount, depositedTokens[_tokenAddress][msg.sender]);
+        emit Deposit(_tokenAddr, msg.sender, _amount, depositedTokens[_tokenAddr][msg.sender]);
     } 
+
+    /// @notice withdraw lets users withdraw their tokens
+    function withdraw(address _tokenAddr, uint256 _amount) public {
+        require(depositedTokens[_tokenAddr][msg.sender] > 0, "You haven't deposited any tokens yet!");
+
+        if (_tokenAddr == address(Token(_tokenAddr))) {
+            Token(_tokenAddr).transfer(msg.sender, _amount);
+            depositedTokens[_tokenAddr][msg.sender] -= _amount;
+        } else if (_tokenAddr == address(SpectreToken(_tokenAddr))) {
+            SpectreToken(_tokenAddr).transfer(msg.sender, _amount);
+            depositedTokens[_tokenAddr][msg.sender] -= _amount;
+        }
+
+        emit Withdraw(_tokenAddr, msg.sender, _amount, depositedTokens[_tokenAddr][msg.sender]);
+    }
 
     /// @notice get the deposited amount of each user for all tokens
     /// @param _token is the token contract address
