f536e2082b1c3cb64db827ef79d01976b21392d9
Arian Hosseini <arianhosseini@outlook.com>
2023-05-13 09:44:37 +0330

    feat: created connection feature / slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import connectionReducer from "../features/connection/connectionSlice";
+
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		connection: connectionReducer,
+	},
 });
---
+++ src/features/connection/connectionSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const connectionSlice = createSlice({
+	name: "connection",
+	initialState,
+	reducers: {},
+});
+
+export default connectionSlice.reducer;
