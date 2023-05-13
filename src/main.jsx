239aeae60931aa74f643aa86da88763293e14c88
Arian Hosseini <arianhosseini@outlook.com>
2023-05-13 07:14:40 +0330

    feat: created the Redux Store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
