68ba7963704f11774d697a588c8f54f7ecb2aed3
Arian Hosseini <arianhosseini@outlook.com>
2023-05-13 15:14:41 +0330

    refactor: refactored loadBlockchainData() & interactions.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-
 import { useDispatch } from "react-redux";
-import { connectionLoaded } from "./features/connection/connectionSlice";
-import { tokensLoaded } from "./features/tokens/tokensSlice";
 
 import { ethers } from "ethers";
 
@@ -12,7 +9,7 @@ import { EthersContext } from "./context/EthersContext";
 import { ExchangeContext } from "./context/ExchangeContext";
 import { TokensContext } from "./context/TokensContext";
 
-import { loadTokens, loadExchange } from "./app/interactions";
+import { loadConnection, loadTokens, loadExchange } from "./app/interactions";
 
 import "./App.css";
 
@@ -24,21 +21,16 @@ const App = () => {
 	const [tokens, setTokens] = useState({});
 
 	const loadBlockchainData = async () => {
-		// Get all the accounts from Metamask
-		const account = await window.ethereum.request({ method: "eth_requestAccounts" }); // makes an RPC call to our node to get our accounts
-
-		// Connect to the Blockchain using Ethers.js
 		// the term "provider" in this case is our connection to the blockchain
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		setProvider(provider);
 
-		const { chainId } = await provider.getNetwork();
-
-		// Save current connection information into the Redux store
-		dispatch(connectionLoaded(chainId, account[0]));
+		// load connections & save the current connection information in the store
+		const { chainId } = await loadConnection(provider, dispatch);
 
 		// Load all tokens contracts
-		const { SPEC, mETH, mDAI, mUSDT } = await loadTokens(provider, [config[chainId]][0]);
+		const tokensConfig = [config[chainId]][0];
+		const { SPEC, mETH, mDAI, mUSDT } = await loadTokens(provider, tokensConfig, dispatch);
 
 		// Save the contracts of all tokens in state & make it globally accessible across the entire app using context
 		setTokens({
@@ -48,18 +40,9 @@ const App = () => {
 			mTether: { symbol: "mUSDT", contract: mUSDT },
 		});
 
-		// Save the information of all tokens inside of the Redux store (except their contracts)
-		dispatch(
-			tokensLoaded({
-				SPEC: config[chainId].spectreToken,
-				mETH: config[chainId].mETH,
-				mDAI: config[chainId].mDAI,
-				mUSDT: config[chainId].mDAI,
-			})
-		);
-
 		// Get the Spectre exchange contract
-		const exchange = await loadExchange(provider, config[chainId].spectre.address);
+		const exchangeConfig = config[chainId].spectre;
+		const exchange = await loadExchange(provider, exchangeConfig.address);
 		setExchange(exchange);
 	};
 
--- src/app/interactions.js
+++ src/app/interactions.js
@@ -4,11 +4,41 @@ import EXCHANGE_ABI from "../abis/Spectre.json";
 import SPECTRE_TOKEN_ABI from "../abis/SpectreToken.json";
 import TOKEN_ABI from "../abis/Token.json";
 
-export const loadTokens = async (provider, addresses) => {
+import config from "../config.json";
+
+import { connectionLoaded } from "../features/connection/connectionSlice";
+import { tokensLoaded } from "../features/tokens/tokensSlice";
+
+export const loadConnection = async (provider, dispatch) => {
+	// Get all the accounts from Metamask
+	const account = await window.ethereum.request({ method: "eth_requestAccounts" }); // makes an RPC call to our node to get our accounts
+
+	// Connect to the Blockchain using Ethers.js
+	const { chainId } = await provider.getNetwork();
+
+	// Save the current connection information into the Redux store
+	dispatch(connectionLoaded(chainId, account[0]));
+
+	return { chainId };
+};
+
+export const loadTokens = async (provider, addresses, dispatch) => {
+	const { chainId } = await provider.getNetwork();
+
 	const SPEC = new ethers.Contract(addresses.spectreToken.address, SPECTRE_TOKEN_ABI, provider);
 	const mETH = new ethers.Contract(addresses.mETH.address, TOKEN_ABI, provider); // accessing to this smart contract
 	const mDAI = new ethers.Contract(addresses.mDAI.address, TOKEN_ABI, provider);
 	const mUSDT = new ethers.Contract(addresses.mUSDT.address, TOKEN_ABI, provider);
+
+	// Save the information of all tokens inside of the Redux store (except their contracts)
+	dispatch(
+		tokensLoaded({
+			SPEC: config[chainId].spectreToken,
+			mETH: config[chainId].mETH,
+			mDAI: config[chainId].mDAI,
+			mUSDT: config[chainId].mDAI,
+		})
+	);
 
 	return { SPEC, mETH, mDAI, mUSDT };
 };
