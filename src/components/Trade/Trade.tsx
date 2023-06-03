6b08e0093967c1d5db5ac82281a6b8d8176ffb75
Arian Hosseini <arianhosseini@outlook.com>
2023-06-03 18:52:31 +0330

    feat: Implemented Exchange Status (DEPOSIT / WITHDRAW) feature

--- src/components/Trade/Trade.tsx
+++ src/components/Trade/Trade.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import TradeRectangle from "../../assets/images/trade-rectangle.svg";
+
+import { Icon } from "..";
 
 import "./Trade.css";
 
+enum Status {
+	WITHDRAW,
+	DEPOSIT,
+}
+
 const Trade = () => {
+	const [exchangeStatus, setExchangeStatus] = useState(Status.WITHDRAW);
+
+	const onExchangeStatusClicked = () => {
+		setExchangeStatus((prevStatus) => (prevStatus === Status.WITHDRAW ? Status.DEPOSIT : Status.WITHDRAW));
+	};
+
 	return (
 		<div className="Trade">
 			<div className="container">
@@ -13,6 +27,23 @@ const Trade = () => {
 						<menu className="Trade__markets">
 							<span className="Trade__markets-label">Market</span>
 						</menu>
+
+						<div className="Trade__status" onClick={onExchangeStatusClicked}>
+							<button
+								className={`Trade__status-btn Trade__status-btn--deposit ${
+									exchangeStatus === Status.DEPOSIT ? "Trade__status-btn--active" : "Trade__status-btn--non-active"
+								}`}
+							>
+								Deposit
+							</button>
+							<button
+								className={`Trade__status-btn Trade__status-btn--withdraw ${
+									exchangeStatus === Status.WITHDRAW ? "Trade__status-btn--active" : "Trade__status-btn--non-active"
+								}`}
+							>
+								Withdraw
+							</button>
+						</div>
 					</section>
 				</div>
 			</div>
