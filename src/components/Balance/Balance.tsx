c875d10fee512ef18668a5df57bf4cac7f0cd86e
Arian Hosseini <arianhosseini@outlook.com>
2023-06-12 20:49:25 +0330

    feat: created Input handlers for deposit transfers

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,13 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, ChangeEvent } from "react";
+import { useImmer } from "use-immer";
 
 import { useAppSelector, useAppDispatch } from "~/app/hooks";
 
 import useFetchGasPrice from "~/hooks/useFetchGasPrice";
+import { EthersContext } from "~/context/EthersContext";
 import { ExchangeContext } from "~/context/ExchangeContext";
 import { TokensContext } from "~/context/TokensContext";
 import { TradeContext } from "~/context/TradeContext";
 
-import { loadBalances } from "~/app/interactions";
+import { loadBalances, transferTokens } from "~/app/interactions";
 
 import SPECLogo from "~/assets/images/spectre-logo-light.png";
 import ETHPreloader from "~/assets/images/preloaders/eth-preloader.gif";
@@ -21,19 +23,53 @@ enum Status {
 }
 
 const Balance = () => {
+	const dispatch = useAppDispatch();
+
 	const { gasFee, transactionFee, fetchGasPrice } = useFetchGasPrice();
 
+	const { provider } = useContext(EthersContext);
 	const { status, handleExchangeMarkets } = useContext(TradeContext);
 	const { exchange } = useContext(ExchangeContext);
 	const { tokens } = useContext(TokensContext);
-
-	const dispatch = useAppDispatch();
 
 	const token1 = useAppSelector(({ tokens }) => tokens.token1);
 	const token2 = useAppSelector(({ tokens }) => tokens.token2);
 	const account = useAppSelector(({ connection }) => connection.current?.account);
 
 	const exchangeBalances = useAppSelector(({ exchange }) => exchange.balances);
+
+	// Inputs values
+	const [token1TransferAmount, setToken1TransferAmount] = useImmer<string>("");
+	const [token2TransferAmount, setToken2TransferAmount] = useImmer<string>("");
+
+	// an event handler to handle the user input
+	const handleAmount = (e: ChangeEvent<HTMLInputElement>, token) => {
+		if (token.address === token1?.address) setToken1TransferAmount(e.target.value);
+		else if (token.address === token2?.address) setToken2TransferAmount(e.target.value);
+	};
+
+	enum TransferType {
+		WITHDRAW = "Withdraw",
+		DEPOSIT = "Deposit",
+	}
+
+	// [x] Step 1: Do the transfer
+	// [x] Step 2: Notify app that transfer is pending
+	// [x] Step 3: Get confirmation from blockchain that transfer was successful
+	// [x] Step 4: Notify app that transfer was successful
+
+	// an event handler when the user clicks the deposit button
+	const handleDeposit = (e: ChangeEvent<HTMLInputElement>, token) => {
+		e.preventDefault();
+
+		if (token.address === token1?.address) {
+			transferTokens(provider, exchange, TransferType.DEPOSIT, tokens[token1?.symbol].contract, token1TransferAmount, dispatch);
+			setToken1TransferAmount("");
+		} else if (token.address == token2?.address) {
+			transferTokens(provider, exchange, TransferType.DEPOSIT, tokens[token2?.symbol].contract, token2TransferAmount, dispatch);
+			setToken2TransferAmount("");
+		}
+	};
 
 	useEffect(() => {
 		if (exchange && account && tokens && token1 && token2)
@@ -68,12 +104,20 @@ const Balance = () => {
 						<h3 className="balance__token-name">{token1?.symbol ?? "SPEC"}</h3>
 					</article>
 
-					<form>
+					<form onSubmit={(e) => handleDeposit(e, token1)} noValidate>
 						<label className="balance__label" htmlFor="token1">
-							AMOUNT
+							{token1 && token1.symbol} AMOUNT
 						</label>
-						<input className="balance__input" type="number" name="token1" placeholder="0.0000" />
-						<button className={`balance__btn balance__btn--${status.toLowerCase()}`} type="button">
+						<input
+							className="balance__input"
+							type="number"
+							name="token1"
+							tabIndex={1}
+							placeholder="0.0000"
+							value={token1TransferAmount}
+							onChange={(e) => handleAmount(e, token1)}
+						/>
+						<button className={`balance__btn balance__btn--${status.toLowerCase()}`} type="submit">
 							<span>{status === Status.WITHDRAW ? Status.WITHDRAW : Status.DEPOSIT}</span>
 							<Icon name="chevron-forward-outline" />
 						</button>
@@ -83,13 +127,13 @@ const Balance = () => {
 						<div className="balance__amounts">
 							<h4 className="balance__exchange" title="Exchange Balance">
 								<img src={SPECLogo} alt="SPEC" />
-								<small>{(exchangeBalances && parseFloat(exchangeBalances.token1)) || 0}</small>
+								<small>{(exchangeBalances && parseFloat(exchangeBalances.token1))?.toFixed(2) || 0}</small>
 								<strong>{token1?.symbol || ""}</strong>
 							</h4>
 							<span></span>
 							<h4 className="balance__wallet" title="Wallet Balance">
 								<Icon name="wallet-outline" />
-								<small>{(token1 && parseFloat(token1?.balance)) || 0}</small>
+								<small>{(token1 && parseFloat(token1?.balance))?.toFixed(2) || 0}</small>
 								<strong>{token1?.symbol || ""}</strong>
 							</h4>
 						</div>
@@ -104,12 +148,20 @@ const Balance = () => {
 						<h3 className="balance__token-name">{token2?.symbol ?? "mETH"}</h3>
 					</article>
 
-					<form>
+					<form onSubmit={(e) => handleDeposit(e, token2)} noValidate>
 						<label className="balance__label" htmlFor="token2">
-							AMOUNT
+							{token2 && token2.symbol} AMOUNT
 						</label>
-						<input className="balance__input" type="number" name="token2" placeholder="0.0000" />
-						<button className={`balance__btn balance__btn--${status.toLowerCase()}`} type="button">
+						<input
+							className="balance__input"
+							type="number"
+							name="token2"
+							tabIndex={2}
+							placeholder="0.0000"
+							value={token2TransferAmount}
+							onChange={(e) => handleAmount(e, token2)}
+						/>
+						<button className={`balance__btn balance__btn--${status.toLowerCase()}`} type="submit">
 							<span>{status === Status.WITHDRAW ? Status.WITHDRAW : Status.DEPOSIT}</span>
 							<Icon name="chevron-forward-outline" />
 						</button>
@@ -119,13 +171,13 @@ const Balance = () => {
 						<div className="balance__amounts">
 							<h4 className="balance__exchange" title="Exchange Balance">
 								<img src={SPECLogo} alt="SPEC" />
-								<small>{(exchangeBalances && parseFloat(exchangeBalances.token2)) || 0}</small>
+								<small>{(exchangeBalances && parseFloat(exchangeBalances.token2))?.toFixed(2) || 0}</small>
 								<strong>{token2?.symbol || ""}</strong>
 							</h4>
 							<span></span>
 							<h4 className="balance__wallet" title="Wallet Balance">
 								<Icon name="wallet-outline" />
-								<small>{(token2 && parseFloat(token2?.balance)) || 0}</small>
+								<small>{(token2 && parseFloat(token2?.balance))?.toFixed(2) || 0}</small>
 								<strong>{token2?.symbol || ""}</strong>
 							</h4>
 						</div>
