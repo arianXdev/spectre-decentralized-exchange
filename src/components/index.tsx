2beb77756257355469f310f66f7146f566c8182f
Arian Hosseini <arianhosseini@outlook.com>
2023-06-24 23:55:17 +0330

    refactor: changed all the component files to index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,10 @@
-export { default as AccountMenu } from "./AccountMenu/AccountMenu";
-export { default as ConnectWallet } from "./ConnectWallet/ConnectWallet";
-export { default as MetaMaskLogo } from "./MetaMaskLogo/MetaMaskLogo";
-export { default as Icon } from "./Icon/Icon";
-export { default as Overlay } from "./Overlay/Overlay";
+export { default as AccountMenu } from "./AccountMenu";
+export { default as ConnectWallet } from "./ConnectWallet";
+export { default as MetaMaskLogo } from "./MetaMaskLogo";
+export { default as Icon } from "./Icon";
+export { default as Overlay } from "./Overlay";
 
-export { default as Markets } from "./Markets/Markets";
-export { default as Balance } from "./Balance/Balance";
-export { default as Order } from "./Order/Order";
-export { default as OrderBook } from "./OrderBook/OrderBook";
+export { default as Markets } from "./Markets";
+export { default as Balance } from "./Balance";
+export { default as Order } from "./Order";
+export { default as OrderBook } from "./OrderBook";
