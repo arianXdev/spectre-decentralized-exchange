bcb512883abdad11e7e3ff740d9a97bb2b18cacf
Arian Hosseini <arianhosseini@outlook.com>
2023-06-25 19:57:50 +0330

    chore: fixed a small type issue

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,11 +1,13 @@
-import { FC, ReactElement } from "react";
+import { FC } from "react";
 
 type iconPropsType = {
 	name: string;
 };
 
-const Icon: FC<iconPropsType> = ({ name }: iconPropsType): ReactElement => {
-	return <ion-icon name={name}></ion-icon>;
+const CustomTag = `ion-icon` as keyof JSX.IntrinsicElements;
+
+const Icon: FC<iconPropsType> = ({ name }) => {
+	return <CustomTag name={name}></CustomTag>;
 };
 
 export default Icon;
