88c05ed9c5622453d554c6823b73838889711400
Arian Hosseini <arianhosseini@outlook.com>
2023-07-17 15:54:47 +0330

    feat: added the current market for the Price Chart, above the Candlestick Chart

--- src/layouts/PriceChart/index.tsx
+++ src/layouts/PriceChart/index.tsx
@@ -6,9 +6,17 @@ const PriceChart = () => {
 	// Get the account from the Redux store
 	const account = useAppSelector((state) => state.connection.current?.account);
 
+	// Get token pairs from the state
+	const tokens = useAppSelector((state) => state.tokens);
+
 	return (
 		<section className="chart">
-			<div className="chart__header"></div>
+			<div className="chart__header">
+				<strong className="chart__token-pair">
+					{tokens && tokens.token1?.symbol} / {tokens && tokens.token2?.symbol}
+				</strong>
+			</div>
+
 			<div className="chart__container">
 				{account ? <p>Candlestick Chart goes here!</p> : <p className="chart__connect-wallet-alert">Please CONNECT YOUR WALLET</p>}
 			</div>
