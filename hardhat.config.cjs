426522f542a27f1751f2f0425ad44658073d79a7
Arian H <arianhosseini@outlook.com>
2023-09-16 19:15:56 +0330

    feat: added SEPOLIA testnet network & INFURA API keys

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,9 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-jest");
 require("dotenv").config();
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const SEPOLIA_PRIVATE_KEYS = process.env.SEPOLIA_PRIVATE_KEYS || "";
+
 module.exports = {
 	solidity: "0.8.21",
 	networks: {
 		localhost: {},
+		sepolia: {
+			url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+			accounts: SEPOLIA_PRIVATE_KEYS.split(","),
+		},
 	},
 };
