3a5f7e2bd75d7bf4aaa110487fd101eba59bf9c7
Arian H <arianhosseini@outlook.com>
2023-09-18 21:19:10 +0330

    feat: added Etherscan API key for source code verification

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const PRIVATE_KEYS = process.env.PRIVATE_KEYS || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
 	solidity: "0.8.21",
@@ -17,5 +18,8 @@ module.exports = {
 			url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
 			accounts: PRIVATE_KEYS.split(","),
 		},
+	},
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
 	},
 };
