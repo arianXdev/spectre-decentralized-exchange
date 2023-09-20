488b6751e1553885facbce7072ad4ef3f6d64d93
Arian H <arianhosseini@outlook.com>
2023-09-21 00:17:12 +0330

    added Alchemy Polygon API KEY

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -3,8 +3,11 @@ require("hardhat-jest");
 require("dotenv").config();
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const PRIVATE_KEYS = process.env.PRIVATE_KEYS || "";
+const ALCHEMY_POLYGON_API_KEY = process.env.ALCHEMY_POLYGON_API_KEY || "";
+
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+const PRIVATE_KEYS = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
 	solidity: "0.8.21",
@@ -16,6 +19,11 @@ module.exports = {
 		},
 		goerli: {
 			url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+			accounts: PRIVATE_KEYS.split(","),
+		},
+		// Polygon testnet
+		polygon: {
+			url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_POLYGON_API_KEY}`,
 			accounts: PRIVATE_KEYS.split(","),
 		},
 	},
