397af62b2811949f38bcd753fecda07614e7f193
Arian H <arianhosseini@outlook.com>
2023-09-22 01:35:20 +0330

    feat: added Binance Smart Chain testnet endpoint

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -6,6 +6,7 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const ALCHEMY_POLYGON_API_KEY = process.env.ALCHEMY_POLYGON_API_KEY || "";
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+// const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY || "";
 
 const PRIVATE_KEYS = process.env.PRIVATE_KEYS || "";
 
@@ -13,6 +14,7 @@ module.exports = {
 	solidity: "0.8.21",
 	networks: {
 		localhost: {},
+		// Ethereum testnets
 		sepolia: {
 			url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
 			accounts: PRIVATE_KEYS.split(","),
@@ -26,8 +28,16 @@ module.exports = {
 			url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_POLYGON_API_KEY}`,
 			accounts: PRIVATE_KEYS.split(","),
 		},
+		// Binance Smart Chain (BSC) testnet
+		bsc: {
+			url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+			chainId: 97,
+			gasPrice: 20000000000,
+			accounts: PRIVATE_KEYS.split(","),
+		},
 	},
 	etherscan: {
 		apiKey: ETHERSCAN_API_KEY,
+		// 	apiKey: BSCSCAN_API_KEY,
 	},
 };
